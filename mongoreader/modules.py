a909874c4f7e1c6ff4acea0135738deb55dedab1
Francesco Garrisi <francesco.garrisi@fiber-resources.com>
2023-10-19 14:38:29 +0200

    Introduction of module collations

---
+++ mongoreader/modules.py
@@ -0,0 +1,15 @@
+import mongomanager as mom
+from mongomanager import log
+import mongoreader.core as c
+import mongoreader.errors as e
+
+class moduleCollation():
+    pass
+
+
+class CDMmoduleCollation(moduleCollation):
+    pass
+
+
+class CDM128moduleCollation(CDMmoduleCollation):
+    pass
